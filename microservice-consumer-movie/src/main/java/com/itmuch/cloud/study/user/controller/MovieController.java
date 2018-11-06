a657f3325f6e298cf317152d9fc899616409ee58
tiger7878 <zhcw63@163.com>
2018-11-06 18:03:34 +0800

    Eureka Server 集群-匿名访问

---
+++ microservice-consumer-movie/src/main/java/com/itmuch/cloud/study/user/controller/MovieController.java
@@ -0,0 +1,24 @@
+package com.itmuch.cloud.study.user.controller;
+
+import com.itmuch.cloud.study.user.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * User: monkey
+ * Date: 2018-11-06 11:44
+ */
+@RestController
+public class MovieController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id){
+        return restTemplate.getForObject("http://localhost:8000/"+id,User.class);
+    }
+}
